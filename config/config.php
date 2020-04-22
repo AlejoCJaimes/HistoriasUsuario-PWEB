f089f9701c73faa9be57061578de2424401d1b13
Juan Alejandro Carrillo Jaimes <juanalejo2010@gmail.com>
2020-04-21 21:29:02 -0500

    Actulizacion .Config
    Se actualizo el nombre de la base de datos, al nombre de la base de datos original que es magil_saberproweb.

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('URL', 'http://localhost/HistoriasUsuario-PWEB/');
 
 define('HOST', 'localhost');
-define('DB', 'pruebna');
+define('DB', 'magil_saberproweb');
 define('USER', 'root');
 define('PASSWORD', "");
 define('CHARSET', 'utf8mb4');
