36a0c36567b3fd8658da2997971598570303dc4c
Juan Alejandro Carrillo Jaimes <juanalejo2010@gmail.com>
2020-04-21 21:21:41 -0500

    Actualización del .Config
    Se realizó un cambio para aclarecer la ruta de la nueva y última versión estable de la base de datos.

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('URL', 'http://localhost/HistoriasUsuario-PWEB/');
 
 define('HOST', 'localhost');
-define('DB', 'magil_saberproweb');
+define('DB', 'pruebna');
 define('USER', 'root');
 define('PASSWORD', "");
 define('CHARSET', 'utf8mb4');
