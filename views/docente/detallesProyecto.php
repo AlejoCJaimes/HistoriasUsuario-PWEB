91ec9e831b97f31cfe5d0233b745295080388557
camilovalncia <camilovalncia@hotmail.com>
2020-04-22 20:46:21 -0500

    Cambios en detallesProyecto
    Acá se realizó cambios para permitir editar con dropdownlist los campos de estado
    El campo de METODOLOGIA falta arreglar el READONLY

--- views/docente/detallesProyecto.php
+++ views/docente/detallesProyecto.php
@@ -7,21 +7,33 @@
     <form action="" method="POST" id="form">
 
         <div class="form-group">
-            <br>
+            
                 <label>Nombre</label>
                 <input type="text" id="nombre" name="NombreProyecto" class="form-control " placeholder="Escribe el nombre del proyecto" value=" " >
+           
         </div>
         <div class="form-group">
                <label>Fecha límite</label>
                <input type="date" id="FechaFin" name="FechaFin" class="form-control"value=" " >
         </div>
-        <div class="form-group">
-                <label>Metodología</label>
-                <input type="text" id="metodología" name="IdMetodologia" class="form-control" value="" >
+
+        <!--DropDrownList de metodología READONLY-->
+        <div class="form-group" readonly> 
+        <label>Metodologia</label readonly>
+        <select name = "metodologia" class="form-control" readonly>
+        <option disabled="disabled" selected="selected">Seleccionar una opcion</option readonly>
+        <option value= "1" selected="selected">Cascada </option readonly>
+        </select>
         </div>
-        <div class="form-group">
-                <label>Estado</label>
-                <input type="text" id="estado" name="IdEstado" class="form-control validar"  value=" ">
+
+
+        <!--DropDrownList de estado-->     
+        <div class="form-group"> 
+        <label>Estado</label>
+        <select name = "estado" class="form-control">
+        <option disabled="disabled" selected="selected">Seleccionar una opcion</option>
+        <option value= "1" selected="selected"> Publicado </option>
+        </select>
         </div>
         <div class="form-group text-center">
                 <input type="submit" id="envio" value="Editar" class="btn btn-primary ">
