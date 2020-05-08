b1651c5fc6caa669fa7a38cec439567a2d92a611
Juan Alejandro Carrillo Jaimes <juanalejo2010@gmail.com>
2020-05-08 16:18:04 -0500

    Update changeclave.php
    > Solo efecto visual de posición.

--- views/administrador/changeclave.php
+++ views/administrador/changeclave.php
@@ -3,23 +3,25 @@
   <h2><i class="fas fa-cog"></i> Cambiar Clave </h2>
        <article>
            <section class="container">
+           <div class="container">
                <form action="<?php echo constant('URL');?>administrador/ActualizarClave" method="post" name="formulario" id="formulario" autocomplete="off">
                    <fieldset>
                       <?php echo $this->confirmacion;?>
                        <div class="form-group">
                          <br>
                          <label>Contraseña</label>
-                        <input type="password" id="validar" name="clave_usuario" class="form-control validar" placeholder="escribe tu contraseña">
+                        <input type="password" id="validar" name="clave_usuario" class="form-control validar" style="width: 450px" placeholder="escribe tu contraseña">
                         </div>
                         <div class="form-group">
                        <label>Confirmar Contraseña</label>
-                       <input type="password" id="nombre" name="confirmar_clave" class="form-control" placeholder="confirma tu contraseña">
+                       <input type="password" id="nombre" name="confirmar_clave" class="form-control" style="width: 450px"  placeholder="confirma tu contraseña">
                        </div>
-                       <div class="form-group text-center">
+                       <div class="form-group">
                        <input type="submit" id="envio" value="Guardar" class="btn btn-primary ">
                        </div>
                    </fieldset>
                </form>
+               </div>
            </section>
        </article>
 </div>
