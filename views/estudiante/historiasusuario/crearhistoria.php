2bded5ae7428b240398e2441e46a9ac5dd3107e8
RangelJhoan <jhoanrangels2@gmail.com>
2020-06-07 21:02:26 -0500

    Actualizacion vista crearhistoria
    > Se actualizó la vista crear usuario para la parte del estudiante

--- views/estudiante/historiasusuario/crearhistoria.php
+++ views/estudiante/historiasusuario/crearhistoria.php
@@ -1,36 +1,50 @@
 <?php require 'views/estudiante/header.php'
 ?>
-
 <!-- INICIO DEL CONTENIDO PRINCIPAL -->
-<form action="<?php echo constant('URL');?>estudiante/CrearHistoria" Method="POST">
-    <table>
-        <thead>
-            <tr>
-                <td>HISTORIA DE USUARIO</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Número: <input type="number"></td>
-                <td>Prioridad: <input type="text"></td>
-                <td>Fecha Creación: <input type="text" value="<?php echo (date("d")-01) . date("F");?>" readonly></td>
-            </tr>
-            <tr>
-                <td>Nombre: <input type="text"></td>
-            </tr>
-            <tr>
-                <td>Descripción: <input type="text"></td>
-            </tr>
-            <tr>
-                <td>Modulo: <select name="" id="">
-                    <option value=""></option>
-                </select></td>
-                <td>Estado: <select name="" id="">
-                    <option value=""></option>
-                </select></td>
-            </tr>
-        </tbody>
-    </table>
+<form action="<?php echo constant('URL');?>estudiante" Method="POST">
+    <div class="container">
+        <table class="table">
+            <thead class="bg-success">
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th class="text-right text-white" scope="col">HISTORIA DE USUARIO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Número: <input type="number" class="form-control col-4"></td>
+                    <td>Prioridad: <input type="text" class="form-control"></td>
+                    <td>Fecha Creación: <input type="text" value="<?php echo (date("d")-01) . " de " . date("F");?>"
+                            class="form-control" readonly>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">Nombre: <input type="text" class="form-control col-6"></td>
+                </tr>
+                <tr>
+                    <td colspan="3">Descripción:<textarea class="form-control" id="exampleFormControlTextarea1"
+                            rows="4"></textarea></td>
+                </tr>
+                <tr>
+                    <td>Módulo: <select name="" id="" class="custom-select">
+                            <option selected disabled>Selecciona un módulo</option>
+                            <option value=""></option>
+                        </select>
+                    </td>
+                </tr>
+                </tr>
+                    <td style="width: 400px;">Estado: <select name="" id="" class="custom-select">
+                            <option selected disabled>Selecciona un estado</option>
+                            <option value=""></option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <input type="submit" class="btn btn-primary">
+    </div>
+
 </form>
 <!-- FIN  DEL CONTENIDO PRINCIPAL -->
 
